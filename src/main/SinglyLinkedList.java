b114ec3dba428a4693fac39738e4c83e95339974
yashagrawal12000 <yash.agrawal_cs17@gla.ac.in>
2019-02-27 14:35:00 +0530

    init commit

--- src/main/SinglyLinkedList.java
+++ src/main/SinglyLinkedList.java
@@ -1,14 +1,19 @@
 package main;
 
 public class SinglyLinkedList {
-    private static class Node{
+    private static class Node {
         private int data;
         private Node next;
 
-        public Node(int data,Node next){
+        public Node(int data, Node next) {
             this.data = data;
             this.next = next;
         }
+    }
+        private int size = 0;
+        private Node head = null;
 
+    public static void main(String[] args) {
+       SinglyLinkedList singlylinkedlist = new SinglyLinkedList();
     }
 }
